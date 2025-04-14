81b88fd0f439a23b56baa894c83f26af88ea54c7
henryph0630 <henryph0630@outlook.com>
2025-04-13 21:25:08 -0400

    added POST

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
-const path = require("path");
 const fs = require("fs");
+const path = require("path");
+const Joi = require("joi");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,12 +10,33 @@ const PORT = process.env.PORT || 3001;
 app.use(cors());
 app.use("/", express.static("public"));
 app.use("/Images", express.static(path.join(__dirname, "public", "Images")));
+app.use(express.json());
 
-// Load costumes data from JSON
 const costumes = JSON.parse(fs.readFileSync(path.join(__dirname, "data", "costumes.json")));
 
 app.get("/api/costumes", (req, res) => {
   res.json(costumes);
+});
+
+const costumeSchema = Joi.object({
+  id: Joi.number().required(),
+  name: Joi.string().required(),
+  price: Joi.string().required(),
+  category: Joi.string().required(),
+  description: Joi.string().required(),
+  Size: Joi.string().required(),
+  img_name: Joi.string().required()
+});
+
+app.post("/api/costumes", (req, res) => {
+  const { error, value } = costumeSchema.validate(req.body);
+  if (error) {
+    return res.status(400).json({ success: false, error: error.details[0].message });
+  }
+
+  costumes.push(value);
+
+  res.json({ success: true, newCostume: value });
 });
 
 app.listen(PORT, () => {
